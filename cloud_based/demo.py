b837422e0bfd5dc22e5f7d85b80db6a30393e816
Sharmila <jinka.s.learning@gmail.com>
2026-01-04 04:34:10 -0500

    CE based

---
+++ cloud_based/demo.py
@@ -0,0 +1,13 @@
+#pip3 install -r requirements.txt
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch(server_name="localhost", server_port= 7860)
